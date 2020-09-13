6070bee7369ea8920a75f2ba265eb54801c0084d
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-09-13 21:10:12 +0900

    api 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends Component {
   state ={
@@ -18,8 +20,29 @@ class App extends Component {
    this.getMovies();
   }
   render() {
-    const {isLoading} = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+         ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie 
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+       )}
+      </section>
+    );
   }
 }
 
